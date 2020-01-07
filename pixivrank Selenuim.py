5789a87319f415eb929a348c82a9f9c812416efd
Caibiner <46318817+Caibiner@users.noreply.github.com>
2020-01-07 20:24:01 +0800

    Update pixivrank Selenuim.py

--- pixivrank Selenuim.py
+++ pixivrank Selenuim.py
@@ -10,10 +10,6 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC 
-from selenium.webdriver.support.wait import WebDriverWait
 from tqdm import tqdm
 
 #获取内容模块
